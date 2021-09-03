d4cbc5c466b7a3e5b6a22d0a63862b7ea0962097
Preston Geyer <prestongeyer713@gmail.com>
2021-09-03 15:13:07 -0400

    server and routes

--- routes.js
+++ routes.js
@@ -1,8 +1,51 @@
 let request = require('request');
+const express = require('express'),
+	Shipment = require('./models/schema');
 
 module.exports = (app) => {
 	// home route - serves angular application
-	app.get("/", (req, res) => {
-		res.sendFile("app.html", { root: __dirname + "/public/html" });
+	app.get('/', (req, res) => {
+		res.sendFile('app.html', { root: __dirname + '/public/html' });
+	});
+
+	app.get('/shipments', async (req, res) => {
+		try {
+			const shipments = await Shipment.find();
+			res.json(shipments);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	});
+
+	app.post('/', async (req, res) => {
+		const {
+			ship_date,
+			tracking_number,
+			shipping_carrier,
+			shipping_method,
+			tracking_url,
+		} = req.body;
+
+		const shipmentFields = {};
+		shipmentFields.ship_date = ship_date;
+		shipmentFields.tracking_number = tracking_number;
+		if (shipping_carrier) shipmentFields.shipping_carrier = shipping_carrier;
+		if (shipping_method) shipmentFields.shipping_method = shipping_method;
+		if (tracking_url) shipmentFields.tracking_url = tracking_url;
+
+		try {
+
+			let shipment = await Shipment.findOneAndUpdate(
+				{ tracking_number: req.body.tracking_number },
+				{ $set: shipmentFields },
+				{ new: true, upsert: true }
+			);
+			await shipment.save();
+			res.json(shipment);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
 	});
 };
--- server.js
+++ server.js
@@ -10,13 +10,21 @@ let port = process.env.PORT || 3000;
 let app = express();
 
 // connect to database (feel free to change the connection info or name of the database)
-mongoose.connect("mongodb://localhost/challengeapp", (err) => {
-	if (err) {
-		console.log("Error connecting to database:", err);
-	} else {
-		console.log("Successfully connected to database!");
+mongoose.connect(
+	process.env.MONGO_URI,
+	{
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useFindAndModify: false,
+	},
+	(err) => {
+		if (err) {
+			console.log('Error connecting to database:', err);
+		} else {
+			console.log('Successfully connected to database!');
+		}
 	}
-});
+);
 
 // mount middleware
 app.use(logger('common'));
@@ -29,8 +37,8 @@ Routes(app);
 // listen for connections
 app.server = app.listen(port, (err) => {
 	if (err) {
-		console.log("Error starting server:", err);
+		console.log('Error starting server:', err);
 	} else {
-		console.log("Server started on port:", port);
+		console.log('Server started on port:', port);
 	}
 });
