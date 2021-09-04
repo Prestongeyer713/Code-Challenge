685b71ddbefa9fa9d1dfe02275dd1ec841c3cadf
Preston Geyer <prestongeyer713@gmail.com>
2021-09-04 12:18:15 -0400

    heroku fix

--- public/js/controller.js
+++ public/js/controller.js
@@ -43,12 +43,13 @@ function getShipmentData() {
 				var obj = data[i];
 
 				$.ajax({
+					mehtod: 'GET',
 					type: 'POST',
+					url: 'https://calm-shelf-58861.herokuapp.com/',
 					data: JSON.stringify(obj),
 					contentType: 'application/json',
 					dataType: 'json',
 					success: console.log('Updating Shipment Data'),
-					url: 'https://calm-shelf-58861.herokuapp.com/',
 				});
 			}
 		}
